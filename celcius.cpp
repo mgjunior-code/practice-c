94c8b36f7a5c5b99faf668386771ad67a778e4e0
Marcelo Junior <36052904+mgjunior-code@users.noreply.github.com>
2023-06-29 22:57:39 -0300

    Create celcius.cpp

---
+++ celcius.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  
+  
+  double tempf = 75.0;
+
+  double tempc;
+  
+  
+  
+  tempc = (tempf - 32)/1.8;
+
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+   
+}
