e2fe6471a44b914b5847f960c22b063f62c04186
Marcelo Junior <36052904+mgjunior-code@users.noreply.github.com>
2023-07-04 23:18:56 -0300

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  
+  double tempf;
+  double tempc;
+  
+  // Ask the user
+  
+  
+  
+  // tempc = (tempf - 32) / 1.8;
+  
+  std::cout << "Enter the temperature in Fahrenheit /n";
+
+  std::cin >> tempf;
+
+  tempc = (tempf - 32) / 1.8;
+
+    std::cout << tempc;
+  
+}
