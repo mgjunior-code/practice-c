2f7a5c0b895a3ae2b8d440a876fdccdfee8f3d72
Marcelo Junior <36052904+mgjunior-code@users.noreply.github.com>
2023-08-01 22:27:17 -0300

    Create size.cpp

---
+++ size.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+    
+  // Add more 
+  grocery.push_back("zelda");
+
+  std::cout << grocery.size();
+  
+  
+  
+  
+}
